903a682a6d43faa8aa71d81d119a4cf4cce2881c
tot0p <thomas.lemaitre.78420@gmail.com>
2025-01-20 12:01:11 +0100

    ajout de middleware

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/tot0p/Hackaton-25-Back
 
 go 1.23.2
 
-require github.com/gofiber/fiber/v2 v2.52.6
+require (
+	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/tot0p/env v0.0.0-20240226095124-cafad61a96a3
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
@@ -11,7 +14,9 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,59 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/utils"
 	"github.com/tot0p/Hackaton-25-Back/internal/handlers"
+	"github.com/tot0p/env"
+	"strconv"
+	"time"
 )
+
+func init() {
+	err := env.Load()
+	if err != nil {
+		return
+	}
+}
 
 func main() {
 	app := fiber.New()
 
+	app.Use(limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.IP() == "127.0.0.1"
+		},
+		Max:        60,
+		Expiration: 30 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Get("x-forwarded-for")
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).SendString("{\"error\": \"Too many requests\"}")
+		},
+	}))
+
+	app.Use(cache.New(cache.Config{
+		ExpirationGenerator: func(c *fiber.Ctx, cfg *cache.Config) time.Duration {
+			newCacheTime, _ := strconv.Atoi(c.GetRespHeader("Cache-Time", "0"))
+			return time.Second * time.Duration(newCacheTime)
+		},
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return utils.CopyString(c.OriginalURL())
+		},
+	}))
+
+	// register routes
 	app.Get("/ping", handlers.PingHandler())
 
-	app.Listen(":8080")
+	// start server
+	port := ":" + env.Get("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+	err := app.Listen(port)
+	if err != nil {
+		return
+	}
 }
